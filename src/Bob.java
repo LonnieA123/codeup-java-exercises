4e6c3e519ea4391bd1e8168829ea196ff7b08afb
Lonnie Arellano <lonnie.j.arellano@gmail.com>
2023-02-13 09:05:33 -0600

    Console work

---
+++ src/Bob.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Bob {
+
+    public static void main(String[] args){
+        System.out.println("hello");
+        do{
+            Scanner scan = new Scanner(System.in);
+            String response = scan.next();
+
+            if(response.endsWith("!")){
+                System.out.println("BROOOOO WHY U YELLLLLLLLLLL");
+
+            }else if(response.endsWith("?")){
+                System.out.println("ya i guess");
+
+            }else if(response.isBlank()){
+                System.out.println("fine be that WAY");
+
+            }else if(response.equals("bye")){
+                System.out.println("cya chump");
+
+            }else{
+                System.out.println("whatEVER");
+            }
+
+        }while (true);
+
+
+
+
+
+
+    }
+}
