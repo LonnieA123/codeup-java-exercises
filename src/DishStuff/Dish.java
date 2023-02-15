2ca8d0009760135f72c1e7f8b70b585c7a32df11
Lonnie Arellano <lonnie.j.arellano@gmail.com>
2023-02-15 11:34:23 -0600

    setters getters and constructors

--- src/DishStuff/Dish.java
+++ src/DishStuff/Dish.java
@@ -1,14 +1,55 @@
 package DishStuff;
-
 public class Dish {
-    public float costInCents;
-    public String nameOfDish;
-    public boolean wouldRecommend;
+
+    private float costInCents;
+    private String nameOfDish;
+    private boolean wouldRecommend;
+
+
+    public float getCost(){
+        return this.costInCents;
+    }
+
+    public String getName(){
+        return this.nameOfDish;
+    }
+    public boolean getRec(){
+        return this.wouldRecommend;
+    }
+
+
+    public void setCost(float cost){
+        this.costInCents = cost;
+    }
+
+    public void setName(String name){
+        this.nameOfDish = name;
+    }
+
+    public void setRec(boolean rec){
+        this.wouldRecommend = rec;
+    }
+
+
+    public Dish(float costInCents, String nameOfDish, boolean wouldRecommend) {
+        this.costInCents = costInCents;
+        this.nameOfDish = nameOfDish;
+        this.wouldRecommend = wouldRecommend;
+        System.out.printf(printSummary());
+    }
+
+    public Dish(){
+        this.costInCents = 000;
+        this.nameOfDish = "unknown";
+        this.wouldRecommend = true;
+        System.out.printf(printSummary());
+    }
 
 
     public String printSummary(){
-        return String.format("Cost: %.2f, Name: %s, Recommended? %b", costInCents,nameOfDish,wouldRecommend);
+        return String.format("Cost: %.2f, Name: %s, Recommended? %b %n", costInCents,nameOfDish,wouldRecommend);
     }
+
 
 }
 
--- src/DishStuff/DishTools.java
+++ src/DishStuff/DishTools.java
@@ -1,28 +1,25 @@
 package DishStuff;
-
 import DishStuff.Dish;
-
 class DishTools{
 
     public static int average = 1300;
 
     public static String shoutDishName(Dish dish){
-        return String.format("THIS IS THE DISH %S",dish.nameOfDish);
+        return String.format("THIS IS THE DISH %S", dish.getName());
 
     };
 
     public static String analyzeDishCost(Dish dish){
-        if (dish.costInCents > average){
-            return String.format("YO THIS IS %.2f EXPENSIve",dish.costInCents);
+        if (dish.getCost() > average){
+            return String.format("YO THIS IS %.2f EXPENSIve",dish.getCost());
         }else{
-            return String.format("only %.2f?!!? YO this good ;)", dish.costInCents);
+            return String.format("only %.2f?!!? YO this good ;)", dish.getCost());
         }
 
     };
 
     public static boolean flipRec(Dish dish){
-
-        return !dish.wouldRecommend;
+        return !dish.getRec();
     }
 
 
